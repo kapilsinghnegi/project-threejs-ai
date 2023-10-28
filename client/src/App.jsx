1cf7ff82f27fa4046b71651c750988c39fcba9f3
Kapil Singh Negi <kapilsinghnegi001@gmail.com>
2023-10-28 19:52:49 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
+"use client";
 import Canvas from './canvas';
 import Customizer from './pages/Customizer';
 import Home from './pages/Home';
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <main className="app transition-all ease-in">
-      <Home />
-      <Canvas />
-      <Customizer />
+      <ErrorBoundary fallback={<p>Something went wrong</p>}>
+        <Home />
+        <Canvas />
+        <Customizer />
+      </ErrorBoundary>
     </main>
   )
 }
